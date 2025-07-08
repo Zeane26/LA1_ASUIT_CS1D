0df232082c6dbf80e3acce2e28f4b5a618e194ac
Zeane26 <zeaneasuit08@gmail.com>
2025-07-09 00:08:36 +0800

    activity2.js

---
+++ activity2.js
@@ -0,0 +1,19 @@
+let firstFavNum = 2; 
+
+let secondFavNum = 6; 
+
+let addition = firstFavNum + secondFavNum;
+let subtraction = firstFavNum - secondFavNum;
+let multiplication = firstFavNum * secondFavNum;
+let division = firstFavNum / secondFavNum;
+let modulus = firstFavNum % secondFavNum;
+let exponentiation = firstFavNum ** secondFavNum;
+
+console.log("First Favorite Number: " + firstFavNum);
+console.log("Second Favorite Number: " + secondFavNum);
+console.log("Addition: " + addition);
+console.log("Subtraction: " + subtraction);
+console.log("Multiplication: " + multiplication);
+console.log("Division: " + division);
+console.log("Modulus: " + modulus);
+console.log("Exponentiation: " + exponentiation);
