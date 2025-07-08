0e99c8e0a9a77536aa989c259d85ec8206142f9e
Zeane26 <zeaneasuit08@gmail.com>
2025-07-09 00:05:23 +0800

    activity1.js

---
+++ activity1.js
@@ -0,0 +1,12 @@
+let name = "Your Name Here"; 
+
+let whatDoYouWannaBecomeInYourLife = "programmer";
+
+let gender = "Your Gender Here";
+
+let twitterHandle = "yourhandle";
+
+console.log("Name: " + name);
+console.log("What I want to become in life: " + whatDoYouWannaBecomeInYourLife);
+console.log("Gender: " + gender);
+console.log("Twitter Handle: " + twitterHandle);
