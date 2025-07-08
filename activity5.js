054538ca292b11ac3373bee03b5a7899abf886c1
Zeane26 <zeaneasuit08@gmail.com>
2025-07-09 00:16:49 +0800

    activity5.js

---
+++ activity5.js
@@ -0,0 +1,17 @@
+let favActorFirstName = "Jackie";
+
+let favActorLastName = "Chan";
+
+let fullName = favActorFirstName + " " + favActorLastName;
+
+let uppercase = fullName.toUpperCase();
+
+let message = "My favorite actor is " + uppercase;
+
+message += ", his best show is Police Story.";
+
+console.log("First Name:", favActorFirstName);
+console.log("Last Name:", favActorLastName);
+console.log("Full Name:", fullName);
+console.log("Uppercase Name:", uppercase);
+console.log("Message:", message);
