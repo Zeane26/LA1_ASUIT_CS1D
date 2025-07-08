b2f7133cfe28a8692c97916363148c899f3d5947
Zeane26 <zeaneasuit08@gmail.com>
2025-07-09 00:13:16 +0800

    activity4.js

---
+++ activity4.js
@@ -0,0 +1,23 @@
+let firstFavNumb = 14; 
+
+let secondFavNumb = "26"; 
+
+let greaterThan = firstFavNumb > secondFavNumb;
+let lessThan = firstFavNumb < secondFavNumb;
+let greaterThanOrEqual = firstFavNumb >= secondFavNumb;
+let lessThanOrEqual = firstFavNumb <= secondFavNumb;
+let strictEquality = firstFavNumb === secondFavNumb; 
+let looseEquality = firstFavNumb == secondFavNumb;   
+let strictInequality = firstFavNumb !== secondFavNumb;
+let looseInequality = firstFavNumb != secondFavNumb;
+
+console.log("First Favorite Number:", firstFavNumb);
+console.log("Second Favorite Number:", secondFavNumb);
+console.log("Greater than:", greaterThan);
+console.log("Less than:", lessThan);
+console.log("Greater than or equal to:", greaterThanOrEqual);
+console.log("Less than or equal to:", lessThanOrEqual);
+console.log("Strict equality (===):", strictEquality);
+console.log("Loose equality (==):", looseEquality);
+console.log("Strict inequality (!==):", strictInequality);
+console.log("Loose inequality (!=):", looseInequality);
