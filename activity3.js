1e5e2b091f63c59139d6538dc998c72c6e007edc
Zeane26 <zeaneasuit08@gmail.com>
2025-07-09 00:10:48 +0800

    activity3.js

---
+++ activity3.js
@@ -0,0 +1,12 @@
+let isJsProgrammingLanguage = true;
+
+let isJsHard = false;
+
+let favNumb = 14; 
+
+let result = favNumb + undefined;
+
+console.log("Is JavaScript a programming language? " + isJsProgrammingLanguage);
+console.log("Is JavaScript hard? " + isJsHard);
+console.log("Favorite Number: " + favNumb);
+console.log("Result of favNumb + undefined: " + result);
